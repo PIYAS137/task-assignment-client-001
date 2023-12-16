a87cea9ddeb0ca120531c1ef66c0be31010f0f5b
S M Piyas Mahamude Alif <piyasmahmudealif@gmail.com>
2023-12-17 03:35:53 +0600

    add new user ui done

--- src/Pages/DashboardScreen/DashboardScreen.jsx
+++ src/Pages/DashboardScreen/DashboardScreen.jsx
@@ -1,16 +1,84 @@
+import { useEffect, useState } from "react";
 import OneRow from "../../Components/OneRow";
 import useGetAllUser from "../../Hooks/useGetAllUser"
 
 const DashboardScreen = () => {
 
   const [userArray, refetch] = useGetAllUser()
+  const [datas, setDatas] = useState([]);
+  const [searchStatus, setSearchStatus] = useState(false)
+  const [sText, setStext] = useState('')
+  const [reverseSatus, setReverseStatus] = useState(true)
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [phone, setPhone] = useState('')
+
+  useEffect(() => {
+    setDatas(userArray);
+  }, [userArray]);
+
+  const handleClickSearch = () => {
+    setSearchStatus(true)
+    const filteredData = userArray.filter(
+      (one) =>
+        one.name.toLowerCase().includes(sText.toLowerCase())
+    );
+    setDatas(filteredData);
+  };
+
+  const handleClickReset = () => {
+    setSearchStatus(false)
+    setDatas(userArray);
+  }
+
+  const handleReverse = () => {
+    setReverseStatus(!reverseSatus)
+    const temp = [...datas].reverse();
+    setDatas(temp);
+  }
+
+  const handleSubmitUser = (e) => {
+    e.preventDefault()
+    const newUser = {
+      name, email, phone
+    }
+    console.log(newUser);
+  }
 
 
   return (
     <>
 
-      <div className=" mt-5 flex justify-end container mx-auto">
-      <button className=" border-none right-3 top-3 btn bg-purple-600 text-sm text-white"> + Add User</button>
+      <div className=" mt-5 flex justify-between container mx-auto">
+        <div className="flex space-x-2">
+          <input onChange={e => setStext(e.target.value)} type="search" placeholder="Search by name" className="input input-bordered w-full max-w-xs" />
+          {searchStatus && <button onClick={handleClickReset} className="btn btn-error">Reset</button>}
+          <button onClick={handleClickSearch} className="btn btn-primary">Search</button>
+          {reverseSatus ?
+            <button onClick={handleReverse} className="btn btn-secondary">Z-A</button>
+            :
+            <button onClick={handleReverse} className="btn btn-secondary">A-Z</button>
+          }
+        </div>
+        <button onClick={() => document.getElementById('my_modal_1').showModal()} className=" border-none right-3 top-3 btn bg-purple-600 text-sm text-white"> + Add User</button>
+
+        {/* Open the modal using document.getElementById('ID').showModal() method */}
+        <div hidden>
+          <form onSubmit={handleSubmitUser} method="dialog">
+            <label className="block text-sm font-bold mt-2">Name</label>
+            <input onChange={e => setName(e.target.value)} type="text" placeholder="Enter Name" className="input input-bordered w-full " />
+            <label className="block text-sm font-bold mt-2">Email</label>
+            <input onChange={e => setEmail(e.target.value)} type="text" placeholder="Enter Email" className="input input-bordered w-full " />
+            <label className="block text-sm font-bold mt-2">Phone</label>
+            <input onChange={e => setPhone(e.target.value)} type="text" placeholder="Enter Phone" className="input input-bordered w-full " />
+            <div className=" py-2 text-center">
+              {/* <button className="btn btn-success text-white mr-2">Save</button> */}
+              <button className="btn">Close</button>
+            </div>
+          </form>
+        </div>
+
+
       </div>
 
       <div className=" bg-red-100 container mx-auto mt-5 rounded-lg">
@@ -26,7 +94,7 @@ const DashboardScreen = () => {
             </thead>
             <tbody>
               {
-                userArray.map(one => <OneRow refetch={refetch} key={one._id} data={one} />)
+                datas?.map(one => <OneRow refetch={refetch} key={one._id} data={one} />)
               }
             </tbody>
           </table>
--- src/Pages/LoginScreen/LoginScreen.jsx
+++ src/Pages/LoginScreen/LoginScreen.jsx
@@ -1,22 +1,50 @@
-import { Link } from "react-router-dom"
+import { useContext, useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import { AuthContext } from "../../Context/FirebaseContext"
+import Swal from "sweetalert2";
 
 
 const LoginScreen = () => {
+
+  const { FirebaseLoginUser } = useContext(AuthContext);
+  const [email, setEmail] = useState('');
+  const [pass, setPass] = useState('');
+  const navigate = useNavigate()
+
+
+  const hanldeSubmitUser = (e) => {
+    e.preventDefault()
+    FirebaseLoginUser(email, pass)
+      .then(res => {
+        if (res.user) {
+          navigate('/')
+          Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: `Successfully Login user !`,
+            showConfirmButton: false,
+            timer: 2000
+          });
+        }
+      })
+  }
+
+
   return (
     <div className=" flex h-screen items-center -mt-16">
-      <form className="card-body max-w-2xl bg-slate-300 mx-auto m-5 rounded-lg">
+      <form onSubmit={hanldeSubmitUser} className="card-body max-w-2xl bg-slate-300 mx-auto m-5 rounded-lg">
         <div className="form-control">
           <h1 className=" font-bold text-2xl text-center ">Login User</h1>
           <label className="label">
             <span className="label-text">Email</span>
           </label>
-          <input type="email" placeholder="email" className="input input-bordered" required />
+          <input onChange={e => setEmail(e.target.value)} value={email} name="email" type="email" placeholder="email" className="input input-bordered" required />
         </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Password</span>
           </label>
-          <input type="password" placeholder="password" className="input input-bordered" required />
+          <input onChange={e => setPass(e.target.value)} value={pass} type="password" placeholder="password" className="input input-bordered" required />
           <label className="label">
             <p className="label-text-alt link link-hover">Don't have an Account? <Link className="font-bold" to={'/signup'}>Create account</Link></p>
           </label>
